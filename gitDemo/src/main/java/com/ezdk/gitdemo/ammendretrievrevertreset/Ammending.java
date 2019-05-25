a3bf79867be7fbbda21c7778e122fb4a6f01fef8
Moataz Sanad <mtz.sanad@gmail.com>
2019-05-25 14:50:20 +0200

    Master conflict 2

--- gitDemo/src/main/java/com/ezdk/gitdemo/ammendretrievrevertreset/Ammending.java
+++ gitDemo/src/main/java/com/ezdk/gitdemo/ammendretrievrevertreset/Ammending.java
@@ -2,12 +2,12 @@ package com.ezdk.gitdemo.ammendretrievrevertreset;
 
 public class Ammending {
 	//Final version master try me
-	//Ammend commit sdf
-	//sdfd fdsfds
-	//change
+	//Ammend commit sdf --deleted //sdsfd
+	//change only in 2
 	//added
 	//another line
 	//master
 	//stop ff
-	//Conflict base
+	//Conflict base thi is conflict 2
+	// master 2 line added
 }
